b89f19eb4c916747dc73474214381e0c5b8dfd7f
liudas123 <liudasp@yahoo.com>
2017-10-04 12:39:03 -0400

    October 4 2017

---
+++ src/supplychain/RestOfPagesController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package supplychain;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author liudas
+ */
+public class RestOfPagesController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
